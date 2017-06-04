3edd929ee32d8632d8bb24210f84fcdee3e2b70a
Michal Swierczek <pwszcpp@gmail.com>
2017-06-04 21:59:33 +0200

    another commit

---
+++ src/main/java/pl/edu/pwsz_oswiecim/inf/unittests/TestyJednostkowe.java
@@ -0,0 +1,60 @@
+package pl.edu.pwsz.student.demo;
+
+/**
+ * Created by mswierczek on 2017-06-04.
+ */
+public class TestyJednostkowe {
+
+    public static class Czlowiek {
+
+        private String imie;
+
+        private String nazwisko;
+
+        private int rokUrodzenia;
+
+        public String getImie() {
+            return imie;
+        }
+
+        public void setImie(String imie) {
+            this.imie = imie;
+        }
+
+        public String getNazwisko() {
+            return nazwisko;
+        }
+
+        public void setNazwisko(String nazwisko) {
+            this.nazwisko = nazwisko;
+        }
+
+        public int getRokUrodzenia() {
+            return rokUrodzenia;
+        }
+
+        public void setRokUrodzenia(int rokUrodzenia) {
+            this.rokUrodzenia = rokUrodzenia;
+        }
+    }
+
+    public String fizzBuzz(int liczba) {
+        if (liczba % 5 == 0) {
+            return "fizz";
+        } else if (liczba % 3 == 0) {
+            return "buzz";
+        } else {
+            return "fizbuzz";
+        }
+    }
+
+    public Czlowiek nowyCzlowiek(String imie, String nazwisko, int rokUrodzenia) {
+        Czlowiek czlowiek = new Czlowiek();
+        czlowiek.setImie(imie);
+        czlowiek.setNazwisko(nazwisko);
+        return czlowiek;
+    }
+
+
+
+}
