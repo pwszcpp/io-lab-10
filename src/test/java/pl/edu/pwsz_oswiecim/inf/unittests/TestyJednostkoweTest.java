b8947bd318f76123d52907f1623d8a8af7fc40f1
Michal Swierczek <pwszcpp@gmail.com>
2018-05-16 17:01:59 +0200

    prepare for labs

---
+++ src/test/java/pl/edu/pwsz_oswiecim/inf/unittests/TestyJednostkoweTest.java
@@ -0,0 +1,12 @@
+package pl.edu.pwsz_oswiecim.inf.unittests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by mswierczek on 2018-05-16.
+ */
+public class TestyJednostkoweTest {
+
+
+}
